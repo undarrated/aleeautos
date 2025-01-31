2903ded5b01ca9c055e86720cecdbf70d284bfb6
Muhd Abdul <muhammadabdullateef738@gmail.com>
2025-01-31 16:46:56 +0100

    a

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
+const express = require('express');
+const app = express();
 const mysql = require('mysql');
 
+// MySQL connection setup
 const connection = mysql.createConnection({
-  host: 'localhost',          // MySQL server location
-  user: 'your_user',          // The user you created or 'root'
-  password: 'your_password',  // The password for the user
-  database: 'car_dealership'  // The name of your database
+  host: 'localhost',
+  user: 'root',
+  password: 'your_password',
+  database: 'car_dealership'
 });
 
 connection.connect((err) => {
@@ -15,4 +18,27 @@ connection.connect((err) => {
   console.log('Connected to MySQL database');
 });
 
-module.exports = connection;
+// Endpoint for searching cars
+app.get('/search', (req, res) => {
+  const { condition, make, model } = req.query; // Capture search params from query string
+
+  // Construct the SQL query based on search parameters
+  let query = 'SELECT * FROM cars WHERE 1=1'; // base query
+  if (condition) query += ` AND condition = '${condition}'`;
+  if (make) query += ` AND make = '${make}'`;
+  if (model) query += ` AND model = '${model}'`;
+
+  connection.query(query, (err, results) => {
+    if (err) {
+      res.status(500).send('Error retrieving data from database');
+      return;
+    }
+    res.json(results); // Send the results back to the frontend
+  });
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
